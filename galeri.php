0ae9528338b5fde2cee17bff0de91d0eb794193a
zeftahost <zeftahosting@gmail.com>
2024-12-13 18:33:39 +0700

    Create galeri.php

---
+++ galeri.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+<main>
+    <h2>Galeri</h2>
+    <div>
+        <img src="assets/images/image1.jpg" alt="Kegiatan 1">
+        <img src="assets/images/image2.jpg" alt="Kegiatan 2">
+    </div>
+</main>
+<?php include 'footer.php'; ?>
