45a20ea08eb04548acdabf35b47f7cf28aded7e2
zeftahost <zeftahosting@gmail.com>
2024-12-14 18:09:10 +0700

    Delete profil.php

--- profil.php
+++
@@ -1,6 +0,0 @@
-<?php include 'header.php'; ?>
-<main>
-    <h2>Profil Sekolah</h2>
-    <p>Selamat datang di halaman profil sekolah. Kami adalah sekolah pusat keunggulan untuk memberikan pendidikan terbaik.</p>
-</main>
-<?php include 'footer.php'; ?>
